218d4032c2dd4b983b7b0bfc88ca468f852d053d
Grigory Kutarev <gk@mactelabs.com>
2017-11-24 14:54:50 +0700

    initial

---
+++ knit2/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+#define GLEW_STATIC
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+
+int main(int argc, char** argv) {
+	if (!glfwInit()) {
+		std::cout << "Failed to init GLFW" << std::endl;
+	}
+	//Настройка GLFW
+	//Задается минимальная требуемая версия OpenGL. 
+	//Мажорная 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	//Минорная
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	//Установка профайла для которого создается контекст
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//Выключение возможности изменения размера окна
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	return 0;
+}
